a28ab5ee9c10d7694d67d562143c4e7c8b304cb2
AdriEspejo <adrieleespejo@gmail.com>
2025-08-31 18:06:17 -0300

    Main
    Agregue un ejemplo para probar la parte 1

---
+++ src/Main/Main.java
@@ -0,0 +1,23 @@
+package Main;
+
+import Main.singleton.DataBase;
+import Main.factory.Libro;
+
+public class Main {
+    public static void main(String[] args) {
+        // Obtenemos la "instancia única" del Singleton
+        DataBase db1 = DataBase.getInstance();
+        DataBase db2 = DataBase.getInstance();
+
+        // Verificamos que es la misma instancia
+        System.out.println("¿Es la misma instancia? " + (db1 == db2));
+
+        // Agregamos libros (usamos db1 y db2 indistintamente, es la misma instancia)
+        db1.agregarLibro(new Libro("El Principito", "Antoine de Saint-Exupéry"));
+        db2.agregarLibro(new Libro("Ficciones", "Jorge Luis Borges"));
+
+        // Listamos los libros (da lo mismo con db1 o db2)
+        System.out.println("Libros en la BD:");
+        db1.getLibros().forEach(System.out::println);
+    }
+}
