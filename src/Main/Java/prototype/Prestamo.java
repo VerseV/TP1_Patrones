f593d7b871f7e39e91c739eb77fceba5d8c65a9e
VickyA7 <vico.ac2507@gmail.com>
2025-09-02 13:22:37 -0300

    Armando Prestamo

--- src/Main/Java/prototype/Prestamo.java
+++ src/Main/Java/prototype/Prestamo.java
@@ -1,4 +1,26 @@
 package Main.Java.prototype;
 
-public class Prestamo {
+import Main.Java.builder.Usuario;
+import Main.Java.factory.Libro;
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class Prestamo implements Cloneable{
+    private Libro libros;
+    private Usuario usuario;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+
+    @Override //sobreescribo metodo que viene de clase padre(Object), para que use esa version y no la por defecto
+              //en este caso sobreescribimos clone(), esta en Object(clase importada automaticamente)
+    public Prestamo clone(){
+        try{
+            return (Prestamo) super.clone(); //SHALLOW
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
