cac9b40417b2436c6caa8978d5269bc2d40448a0
AdriEspejo <adrieleespejo@gmail.com>
2025-09-02 22:42:28 -0300

    LogisticaLibro
    use equalsIgnoreCase en lugar de equals para aceptar mayusculas y minisculas

--- src/Main/Java/factory/LogisticalLibro.java
+++ src/Main/Java/factory/LogisticalLibro.java
@@ -3,7 +3,7 @@ package Main.Java.factory;
 public class LogisticalLibro {
     
     public static Libro crearLibro(String tipo, String titulo, String autor) {
-        if (tipo.equals("Digital")) return new LibroDigital(titulo, autor);
+        if (tipo.equalsIgnoreCase("Digital")) return new LibroDigital(titulo, autor);
         else return new LibroFisico(titulo, autor);
     }
 }
